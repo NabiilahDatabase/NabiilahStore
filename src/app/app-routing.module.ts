05e6090e20b549ca1dcb626e0ac4b541ebc1890e
nabiilahdatabase <nabiilahdatabase@gmail.com>
2019-05-27 21:06:53 +0700

    firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,11 @@ import { auth } from 'firebase/app';
 export class LoginPage implements OnInit {
   username = '';
   password = '';
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public alert: AlertController,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -19,15 +25,27 @@ export class LoginPage implements OnInit {
     const { username, password } = this;
     try {
       const res = await this.afAuth.auth.signInWithEmailAndPassword(username, password);
+      console.log(res);
+      this.router.navigate(['/main']);
     } catch (err) {
       console.dir(err);
       if (err.code === 'auth/user-not-found') {
         console.log('User tidak ditemukan');
+        this.showAlert('Error!', 'Username atau Password salah!');
       }
     }
   }
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 }
