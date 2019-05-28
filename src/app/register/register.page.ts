ee2b3ef2a1b246114f2c9dd26a477a46d7fcb7f2
NabiilahDatabase <nabiilahdatabase@gmail.com>
2019-05-28 14:27:08 +0700

    navigasi-page

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
   hp = '';
   email = '';
   password = '';
   upassword = '';
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public alert: AlertController,
+    public router: Router
+    ) { }
 
   ngOnInit() {
   }
@@ -21,14 +28,27 @@ export class RegisterPage implements OnInit {
     const { hp, email, password, upassword } = this;
 
     if (password !== upassword) {
+      this.showAlert('Error', 'Password yg anda masukkan salah!');
       return console.error('Password tidak sama!');
     }
 
     try {
       const res = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
       console.log(res);
+      this.showAlert('Sukses!', 'Akun ' + email + ' berhasil dibuat');
+      this.router.navigate(['/login']);
     } catch (err) {
       console.dir(err);
+      this.showAlert('Error!', err.message);
     }
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 }
