8e255e7e9a83ea69cc95214706c297e93c7d2d2f
NabiilahDatabase <nabiilahdatabase@gmail.com>
2019-05-29 16:33:53 +0700

    upload-file

--- src/app/akun/akun.page.ts
+++ src/app/akun/akun.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-akun',
   templateUrl: './akun.page.html',
   styleUrls: ['./akun.page.scss'],
 })
+
 export class AkunPage implements OnInit {
 
-  constructor() { }
+  public imageURL: string;
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  fileChanged(event) {
+    const files = event.target.files;
+
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('UPLOADCARE_STORE', '1');
+    data.append('UPLOADCARE_PUB_KEY', '8f7cf0c54abcd5149cf5');
+
+    this.http.post('https://upload.uploadcare.com/base/', data)
+    .subscribe( events => {
+      console.log(events),
+      this.imageURL = events['file'];
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,9 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,11 +27,13 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireStorageModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClientModule,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
