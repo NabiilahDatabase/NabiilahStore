dc98831c75623e7793843b169a0a305840d046ee
nabiilahdatabase <nabiilahdatabase@gmail.com>
2019-06-21 23:29:56 +0700

    firefunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+export const getPost = functions.https.onRequest(async (req, res) => {
+    const docs = await admin.firestore().collection('produk').limit(10).get();
+    res.send(docs.docs.map(doc => doc.data()));
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
